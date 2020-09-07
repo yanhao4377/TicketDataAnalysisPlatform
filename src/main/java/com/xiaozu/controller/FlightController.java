11fb1aa1c6d8bc8a638de903539dc71d6438f4ed
yanhao4377 <54231661+yanhao4377@users.noreply.github.com>
2020-09-07 13:13:15 +0800

    Update FlightController.java

--- src/main/java/com/xiaozu/controller/FlightController.java
+++ src/main/java/com/xiaozu/controller/FlightController.java
@@ -2,6 +2,8 @@ package com.xiaozu.controller;
 
 import com.xiaozu.entity.Dog;
 import com.xiaozu.entity.Flight;
+import com.xiaozu.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping
 public class FlightController {
+        @Autowired
+        private FlightService flightService;
+
         @RequestMapping("hh")
         String test(HttpServletRequest request){
             request.setAttribute("test","东方航空");
@@ -21,9 +27,8 @@ public class FlightController {
 
         @RequestMapping("Route/searchRoute")
         @ResponseBody
-        Dog searchRoute(@RequestParam("start") String start, @RequestParam("end") String end){
-            System.out.println(start+"  "+end);
-            Dog dog = new Dog("cathie",3);
-            return dog;
+        ArrayList<Flight> searchRoute(@RequestParam("start") String start, @RequestParam("end") String end){
+            ArrayList<Flight> flights = flightService.selectBySaE(start, end);
+            return  flights;
         }
 }
